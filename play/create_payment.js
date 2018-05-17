37116d2e91a2bcef947e0c2c6fceabf6163f70ce
liuqixing <liuqixing@gmail.com>
2018-05-17 12:39:18 +0800

    rm .idea

--- play/create_payment.js
+++ play/create_payment.js
@@ -1,30 +1,37 @@
 /*jslint node: true */
 "use strict";
+
 var headlessWallet = require('../start.js');
 var eventBus = require('byteballcore/event_bus.js');
 
-function onError(err){
-	throw Error(err);
+
+function onError( err )
+{
+	throw Error( err );
 }
 
-function createPayment(){
-	var composer = require('byteballcore/composer.js');
-	var network = require('byteballcore/network.js');
-	var callbacks = composer.getSavingCallbacks({
-		ifNotEnoughFunds: onError,
-		ifError: onError,
-		ifOk: function(objJoint){
-			network.broadcastJoint(objJoint);
+function createPayment()
+{
+	var composer	= require('byteballcore/composer.js');
+	var network	= require('byteballcore/network.js');
+	var callbacks	= composer.getSavingCallbacks
+	({
+		ifNotEnoughFunds	: onError,
+		ifError			: onError,
+		ifOk			: function( objJoint )
+		{
+			network.broadcastJoint( objJoint );
 		}
 	});
-	
-	var from_address = "PYQJWUWRMUUUSUHKNJWFHSR5OADZMUYR";
-	var payee_address = "LS3PUAGJ2CEYBKWPODVV72D3IWWBXNXO";
-	var arrOutputs = [
-		{address: from_address, amount: 0},      // the change
-		{address: payee_address, amount: 10000}  // the receiver
+
+	var from_address	= "PYQJWUWRMUUUSUHKNJWFHSR5OADZMUYR";
+	var payee_address	= "LS3PUAGJ2CEYBKWPODVV72D3IWWBXNXO";
+	var arrOutputs		=
+	[
+		{ address: from_address, amount: 0 },		//	the change
+		{ address: payee_address, amount: 10000 }	//	the receiver
 	];
-	composer.composePaymentJoint([from_address], arrOutputs, headlessWallet.signer, callbacks);
+	composer.composePaymentJoint( [ from_address ], arrOutputs, headlessWallet.signer, callbacks );
 }
 
-eventBus.on('headless_wallet_ready', createPayment);
+eventBus.on( 'headless_wallet_ready', createPayment );
